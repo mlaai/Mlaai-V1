51f1f1a6cc178ff42a9791e4f2e808b229a86c03
Anurag Sareen <anurag.sareen@assurant.com>
2019-11-18 20:08:18 -0500

    Added model and predict

--- Mlaai/V1/helper.py
+++ Mlaai/V1/helper.py
@@ -85,7 +85,7 @@ class helper(object):
             if i % 100 == 0:
                 costs.append(cost)
 
-            if print_cost and i % 100 = 0:
+            if print_cost and i % 100 == 0:
                 print("Cost after iteration %i: %f" %(i, cost))
 
         params = {"w": w,
@@ -94,3 +94,56 @@ class helper(object):
         grads = {"dw": dw,
                   "db": db}
 
+        return params, grads, costs
+
+    def predict(w, b, X):
+        """
+        Predict label 0 or 1 using learned logistic regression paramters: w and b
+        """
+        m = X.shape[1]
+        Y_prediction = np.zeros((1,m))
+        w = w.reshape(X.shape[0], 1)
+
+        A = sigmoid(np.dot(w.T,X) + b)
+
+        for i in range(A.Shape[1]):
+            if A[0, i] <= 0.5:
+                Y_prediction[0, i] = 0
+            else:
+                Y_prediction[0, i] = 1
+
+        return Y_prediction
+
+    def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+        """
+        Builds the logistic regression model
+        """
+
+        # 1. Initialize weights and bias. 
+        # Size is calculated form the Input Training dataset and weights are initialized to zeros for all the training dataset values
+        # Bias is initialized to zero
+        w = initializeWithZeros(X_train.shape[0])
+        b = 0
+
+        # 2. Calculate the gradient descent and costs
+        parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost = print_cost)
+    
+        w = parameters["w"]
+        b = parameters["b"]
+
+        Y_prediction_test = predict(w, b, X_test)
+        Y_prediction_train = predict(w, b, X_train)
+
+        print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+        print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    
+        d = {"costs": costs,
+             "Y_prediction_test": Y_prediction_test, 
+             "Y_prediction_train" : Y_prediction_train, 
+             "w" : w, 
+             "b" : b,
+             "learning_rate" : learning_rate,
+             "num_iterations": num_iterations}
+
+        return d
